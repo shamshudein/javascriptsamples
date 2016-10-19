7111246875d37c0bb489c69de943435fac66f0b9
shamshudein <ShamshudeinSyed.Kareem@cognizant.com>
2016-10-19 17:53:53 +0530

    sample js project

---
+++ js/index.js
@@ -0,0 +1,15 @@
+$(document).ready(
+    function () {
+        console.log("jquery initiated");
+        console.log("jquery2 initiated");
+        console.log("jquery3 initiated");
+    });
+
+function success()
+{
+
+}
+function fail()
+{
+
+}
